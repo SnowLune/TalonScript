a94a76ad68d5d817daf6aefc0b9b2f3c10dfc5ff
SnowRoberge <SnowRoberge@gmail.com>
2020-03-15 18:48:16 -0500

    add/update files. created workshop_values_expects.json

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const values = jQuery.getJSON("workshop_values.json")
+// $.getJSON("https://raw.githubusercontent.com/SnowRoberge/TalonScript/master/workshop_values.json");
+
+let JSONURL = "https://raw.githubusercontent.com/SnowRoberge/TalonScript/master/workshop_values.json";
+let request = new XMLHttpRequest();
+request.open('GET', JSONURL);
+request.responseType = 'text';
+request.send();
+
+request.onload = function() {
+    const workshopValues_text = request.response;
+    const workshopValuesJS = JSON.parse(workshopValues_text);
+}
+
+const workshopValues = $.map(workshopValuesJS, function(element, index){
+    return index;
+});
